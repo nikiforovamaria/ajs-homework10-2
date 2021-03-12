1d580d4cf0b97713bb1206767505d721c6c04e9d
Maria Nikiforova <nikiforovamary@gmail.com>
2021-03-12 19:33:04 +0600

    third commit

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,6 +1,13 @@
 import GameSavingLoader from '../GameSavingLoader';
 
 test('1 - should return object', async () => {
+  const result = {
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1, name: 'Hitman', level: 10, points: 2000,
+    },
+  };
   const game = await GameSavingLoader.load();
-  expect(game).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+  expect(game).toEqual(result);
 });
